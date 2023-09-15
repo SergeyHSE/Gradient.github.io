a9d4f5c6d9dc48e3884eed816834d16b5bfc2a41
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-09-15 17:38:49 +0300

    Update StochasticGradient.py

--- StochasticGradient.py
+++ StochasticGradient.py
@@ -2,3 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
+from sklearn.base import BaseEstimator
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+pd.set_option('display.max_columns', None)
+
+path = r"YourPath"
+path = path.replace('\\', '/')
+data = pd.read_csv(path)
+data.head()
+X = data[
+
