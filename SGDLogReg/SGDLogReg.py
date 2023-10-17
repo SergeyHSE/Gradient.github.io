4df05c7f3962f80c6cc8048c33ab51a53fbd379e
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2023-10-17 12:06:14 +0300

    Update SGDLogReg.py

--- SGDLogReg/SGDLogReg.py
+++ SGDLogReg/SGDLogReg.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 20:18:07 2023
+
+@author: SergeyHSE
+"""
+
+import numpy as np
 import pandas as pd
+from scipy.special import expit
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.base import ClassifierMixin
